23b5eada49f3ac0237d6ad0123ccf7cdf4cd03da
Wolfgang Vandevenne <woll0r@gmail.com>
2014-10-10 19:41:58 +0200

    Use the correct git commands

--- hook.sh
+++ hook.sh
@@ -11,12 +11,13 @@ WEBDIR=/var/www/emotes.cardboardbox.be
 
 # Update emotes
 cd $EMOTEPATH
+git checkout master
 git remote update
 git pull --all
 
 for branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
     # Switch to branch
-    git branch $branch
+    git checkout $branch
 
     # Run Tada
     $VIRTUALENV/bin/python $TADAPATH/main.py -i $EMOTEPATH -o $WEBDIR/output
